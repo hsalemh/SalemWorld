23632cfd3da17932af6c87c2cb92fcdfd3abc416
hsalemh <salem.hamiane@outlook.fr>
2025-12-30 13:50:08 +0100

    update

--- SalemWorld/src/views/MyGoals.vue
+++ SalemWorld/src/views/MyGoals.vue
@@ -17,8 +17,7 @@
             <div class="row">
               <div class="col-md-6 mb-3">
                 <h5 class="text-primary">Court terme</h5>
-                <p>- Décrocher un poste en Alternance dans mes domaines de compétences. <br>
-                  - Maitriser les outils DevOps. <br>
+                <p>
                   - Obtenir des certifications AWS, AZURE, CISCO, TryHackMe. <br>
                   - Obtenir Mon diplôme d'ingénieur.
                 </p>
